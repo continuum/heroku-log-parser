840c64de47e5423a3105e216671bc4983e04e7ad
Leonardo Soto M <leo.soto@gmail.com>
2013-11-18 13:42:09 -0300

    Support timestamps without fractions of seconds
    (as generated by heroku-postgres)

--- lib/heroku-log-parser.rb
+++ lib/heroku-log-parser.rb
@@ -21,7 +21,7 @@ class HerokuLogParser
     # msgid [no structured data = '-'] [something else they added recently = '-'] msg
     # 120 <40>1 2013-07-26T18:39:37.489572+00:00 host app web.11 - - State changed from starting to up.
     def line_regex
-      @line_regex ||= /\<(\d+)\>(1) (\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d\d\d\d\+00:00) ([a-z0-9\-\_\.]+) ([a-z0-9-]+) ([a-z0-9\-\_\.]+) (\-) (\-) (.*)$/
+      @line_regex ||= /\<(\d+)\>(1) (\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(?:\.\d\d\d\d\d\d)?\+00:00) ([a-z0-9\-\_\.]+) ([a-z0-9-]+) ([a-z0-9\-\_\.]+) (\-) (\-) (.*)$/
     end
 
     # Heroku's http log drains (https://devcenter.heroku.com/articles/labs-https-drains)
